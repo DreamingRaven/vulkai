4d587011ee4a7116b2ecef473dfd50e627ac63c0
DreamingRaven <GeorgeRavenNR@outlook.com>
2019-06-25 00:47:52 +0100

    Added CMakeLists.txt with vulkan find
    Cmake lists should now find vulkan for us although it has been some time
    since I touched C++ so im metalf farting repeatedley

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+/**
+ * @Author: archer
+ * @Date:   2019-06-25T00:31:11+01:00
+ * @Last modified by:   archer
+ * @Last modified time: 2019-06-25T00:40:00+01:00
+ */
+
+#include <iostream>
+#include "vulkan.h"
+
+int main() {
+    // try{
+    //     VulkanEngine app;
+    //     app.run();
+    // }
+    // catch(const std::runtime_error& e) {
+    //     std::cerr << e.what() << std::endl;
+    //     return EXIT_FAILURE;
+    // }
+    return EXIT_SUCCESS; // 0
+}
